21655f6b414a107c6919405ec40adc24d01445ab
ChukwudiNebo <chukwudineboch@gmail.com>
2023-04-08 10:14:01 -0700

    created a for loop to return a new array

---
+++ script.js
@@ -0,0 +1,22 @@
+// const array=['chukwudi',3,'rice'];
+
+// // console.log(map(array,(array)=>{array}));
+// const mapped = map(array,(array)=>{array})
+// (mapped)();
+
+
+// (() => {
+  
+// })();
+let arr =['sdfds']
+for(let i=0;i<arr.length;i++){
+    let array=[]
+    console.log(arr[i]+'klsdf')
+    // return array
+
+
+// i pushed 2 to the new array 
+    // array.push(i+2)
+    // console.log(array)
+}
+
