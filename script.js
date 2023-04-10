ab21658e2996475cc2d6586b3ce48d18d4ff275f
ChukwudiNebo <chukwudineboch@gmail.com>
2023-04-10 08:15:51 -0700

    added script

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-// const array=['chukwudi',3,'rice'];
+// Used a function to accept the array
+const arrFunction = (arr) => {
+  let array = [];
+  for (let i = 0; i < arr.length; i++) {
+    const pushArr = array.push(arr[i] * 2);
+  }
+  // console.log(array);
+  return array;
+};
+console.log(arrFunction([1, 2, 3, 4]));
 
-// // console.log(map(array,(array)=>{array}));
-// const mapped = map(array,(array)=>{array})
-// (mapped)();
-
-
-// (() => {
-  
-// })();
-let arr =['sdfds']
-for(let i=0;i<arr.length;i++){
-    let array=[]
-    console.log(arr[i]+'klsdf')
-    // return array
-
-
-// i pushed 2 to the new array 
-    // array.push(i+2)
-    // console.log(array)
+// i didn't use a function
+let numArr = [1, 4, 9];
+let Numarray = [];
+for (let i = 0; i < numArr.length; i++) {
+  Numarray.push(Math.sqrt(numArr[i]));
 }
+console.log(Numarray);
 
+// modifying the objects in an array
+const reformatArray = (objArr) => {
+  const { key, value } = objArr;
+  let formatArr = [];
+  for (let index = 0; index < objArr.length; index++) {
+    // console.log([objArr[index].key,objArr[index].value])
+    // formatArr.unshift([objArr[index].key,objArr[index].value])
+    formatArr.unshift(objArr[index].key, objArr[index].value);
+  }
+  console.log(formatArr);
+};
+
+console.log(
+  reformatArray([
+    { key: 1, value: 10 },
+    { key: 2, value: 20 },
+    { key: 3, value: 39 },
+  ])
+);
